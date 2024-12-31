62d75e59d43b041b55bdf8b6fe3af0431198913b
Dalaali Gousiya <gousiyabhanu3302@gmail.com>
2024-12-31 15:38:40 +0530

    changed

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,7 @@
-class Main{
-	public static void main(String[] args){
+class Main{ #from dev2
+	public static void main(String[] args){ #from dev2
 		System.out.println("Hello from Dev1");
+		System.out.println("Hello from Dev2");
 	}
 }
 
